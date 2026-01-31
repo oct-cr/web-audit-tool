440bd71fe13d93afdd47d2681cb69b995f960f72
Octavio <oc@octaviocoria.com>
2026-01-31 00:13:28 -0300

    add Sphinx Documentation Generator

---
+++ docs/conf.py
@@ -0,0 +1,31 @@
+import os
+import sys
+
+sys.path.insert(0, os.path.abspath(".."))
+sys.path.insert(0, os.path.abspath("../src"))
+
+project = "Web Audit Tool"
+copyright = "2026"
+author = "Octavio Coria"
+
+extensions = [
+    "sphinx.ext.autodoc",
+]
+
+autodoc_default_options = {
+    "members": True,
+    "member-order": "bysource",
+    "undoc-members": True,
+    "show-inheritance": True,
+}
+
+toc_object_entries_show_parents = "all"
+
+templates_path = ["_templates"]
+exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
+
+html_theme = "sphinxdoc"
+html_theme_options = {
+    "nosidebar": True,
+}
+html_static_path = ["_static"]
---
+++ docs/index.rst
@@ -0,0 +1,13 @@
+Web Audit Tool
+==============
+
+.. toctree::
+   :maxdepth: 10
+
+   api/src
+
+Indices
+-------
+
+* :ref:`genindex`
+* :ref:`modindex`
